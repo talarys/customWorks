f7aa5fd61a3df2003d24bf43c3b25dee9315ad6d
talarys <d.simon.ssx0@gmail.com>
2023-07-25 19:07:48 +0200

    Fixed missing class

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,14 +11,12 @@ import {
 
 import { CustomButton } from '../components'
 import state from '../store'
-
 const Home = () => {
   const snap = useSnapshot(state)
 
   return (
     <AnimatePresence>
       {snap.intro && (
-        // Logo
         <motion.section className='home' {...slideAnimation('left')}>
           <motion.header {...slideAnimation('down')}>
             <img
@@ -27,26 +25,31 @@ const Home = () => {
               className='w-8 h-8 object-contain'
             />
           </motion.header>
-          {/* Lets do it */}
-          <motion.div {...headTextAnimation}>
-            <h1 className='head-text'>
-              LET'S <br className='xl:block hidden' /> DO IT.
-            </h1>
+
+          <motion.div className='home-content' {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className='head-text'>
+                LET'S <br className='xl:block hidden' /> DO IT.
+              </h1>
+            </motion.div>
+            <motion.div
+              {...headContentAnimation}
+              className='flex flex-col gap-5'
+            >
+              <p className='max-w-md font-normal text-gray-600 text-base'>
+                Create your unique and exclusive shirt with our brand-new 3D
+                customization tool. <strong>Unleash your imagination</strong>{' '}
+                and define your own style.
+              </p>
+
+              <CustomButton
+                type='filled'
+                title='Customize It'
+                handleClick={() => (state.intro = false)}
+                customStyles='w-fit px-4 py-2.5 font-bold text-sm'
+              />
+            </motion.div>
           </motion.div>
-          {/* Random marketing */}
-          <motion.div {...headContentAnimation} className='flex flex-col gap-5'>
-            <p className='max-w-md font-normal text-gray-600 text-base'>
-              Create your unique and exclusive shirt with out brand new 3D
-              customization tool. <strong>Unleash your imagination</strong> and
-              define your own style
-            </p>
-          </motion.div>
-          <CustomButton
-            type='filled'
-            title='Customize It'
-            handleClick={() => (state.intro = false)}
-            customStyles='w-fit px-4 py-2.5 font-bold text-sm'
-          />
         </motion.section>
       )}
     </AnimatePresence>

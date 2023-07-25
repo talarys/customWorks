71382386aca1f719a47ec2853c508baea58fae7c
talarys <d.simon.ssx0@gmail.com>
2023-07-25 22:27:23 +0200

    Finished layout

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -19,7 +19,57 @@ import {
 } from '../components'
 
 const Customizer = () => {
-  return
+  const snap = useSnapshot(state)
+
+  return (
+    <AnimatePresence>
+      {!snap.intro && (
+        <>
+          {/* Left sidebar */}
+          <motion.div
+            key='custom'
+            className='absolute top-0 left-0 z-10'
+            {...slideAnimation('left')}
+          >
+            <div className='flex items-center min-h-screen'>
+              <div className='editortabs-container tabs'>
+                {EditorTabs.map((tab) => (
+                  <Tab key={tab.name} tab={tab} handleClick={() => {}} />
+                ))}
+              </div>
+            </div>
+          </motion.div>
+          {/* Go Back Button */}
+          <motion.div
+            className='absolute z-10 top-5 right-5'
+            {...fadeAnimation}
+          >
+            <CustomButton
+              type='filled'
+              title='Go Back'
+              handleClick={() => (state.intro = true)}
+              customStyle='w-fit px-4 py-2.5 font-bold text-sm'
+            />
+          </motion.div>
+          {/* Bottom Bar */}
+          <motion.div
+            className='filtertabs-container'
+            {...slideAnimation('up')}
+          >
+            {EditorTabs.map((tab) => (
+              <Tab
+                key={tab.name}
+                tab={tab}
+                isFilterTab
+                isActiveTab=''
+                handleClick={() => {}}
+              />
+            ))}
+          </motion.div>
+        </>
+      )}
+    </AnimatePresence>
+  )
 }
 
 export default Customizer

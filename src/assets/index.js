7cad0f0390f28e678bd9db6a52fed96c961e150b
talarys <d.simon.ssx0@gmail.com>
2023-07-25 17:43:02 +0200

    Fixed assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,9 +1,17 @@
-import ai from "./ai.png";
-import fileIcon from "./file.png";
-import swatch from "./swatch.png";
-import download from "./download.png";
+import aiIcon from './ai.png'
+import downloadIcon from './download.png'
+import fileIcon from './file.png'
+import swatchIcon from './swatch.png'
+import logoShirtIcon from './logo-tshirt.png'
+import stylishShirtIcon from './stylish-tshirt.png'
+import threeJsIcon from './threejs.png'
 
-import logoShirt from "./logo-tshirt.png";
-import stylishShirt from "./stylish-tshirt.png";
-
-export { ai, fileIcon, swatch, download, logoShirt, stylishShirt };
+export {
+  aiIcon,
+  downloadIcon,
+  fileIcon,
+  swatchIcon,
+  logoShirtIcon,
+  stylishShirtIcon,
+  threeJsIcon,
+}
